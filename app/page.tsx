52b88bcdf2934601062c46fb2b4247f0cb971ddf
torukino <toruk0131@gmail.com>
2023-12-31 21:44:54 +0900

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import NozomiLogo from '@/public/nozomi/nozomi-logo'
 import Nav from '@/app/Nav'
+import { Button } from '@/components/ui/button'
 
 export default function Page() {
 	return (
-		<main className="flex h-screen flex-col p-6">
-			<div className="w-full h-full">
-				<div className="h-20 items-end rounded-lg bg-gray-100 p-4 md:w-64">
-					<NozomiLogo />
-				</div>
-
-				<div className="h-full flex flex-col md:flex-row rounded-lg bg-gray-50 px-6 py-10 ">
-					<Nav />
-
-					<div className="bg-yellow-100 flex flex-grow items-center justify-center p-6 md:w-3/5">@@@@@本文 HOME@@@@@</div>
-				</div>
-			</div>
-		</main>
+		<div>
+			<Button size="lg">
+				root
+			</Button>
+		</div>
 	)
 }

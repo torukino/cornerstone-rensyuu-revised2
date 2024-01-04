534b101821eb33037aeaf41b4502715403f8cde5
torukino <toruk0131@gmail.com>
2024-01-05 05:40:53 +0900

    Add user update functionality to settings

--- actions/settings.ts
+++ actions/settings.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcryptjs'
 import type { SettingsSchema } from '@/schemas'
 import type * as z from 'zod'
 
+import { update } from '@/auth'
 import { getUserByEmail, getUserById } from '@/data/user'
 import { currentUser } from '@/lib/auth'
 import { db } from '@/lib/db'
@@ -55,7 +56,7 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
 		values.newPassword = undefined
 	}
 
-	await db.user.update({
+	const updatedUser = await db.user.update({
 		where: {
 			id: dbUser.id,
 		},
@@ -64,5 +65,13 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
 		},
 	})
 
+	update({
+		user: {
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
+			role: updatedUser.role,
+		},
+	})
 	return { success: 'Settings updated!' }
 }
--- auth.ts
+++ auth.ts
@@ -16,6 +16,7 @@ export const {
 	auth,
 	signIn,
 	signOut,
+	update,
 } = NextAuth({
 	pages: {
 		signIn: '/auth/login',
